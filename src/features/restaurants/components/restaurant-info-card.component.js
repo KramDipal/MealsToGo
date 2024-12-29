61618ad4b69806f419d0acdc22c92d573ce386c9
KramDipal <francis.lapid@gmail.com>
2024-12-29 18:23:05 +0800

    Added SVG's 1. Open 2. lodging icon 3. Info 4. Rating 5. Section 6. SectionEnd 7. ratingArray

--- src/features/restaurants/components/restaurant-info-card.component.js
+++ src/features/restaurants/components/restaurant-info-card.component.js
@@ -1,7 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import { Text, StyleSheet } from "react-native";
+import { Text, Image, View } from "react-native";
 import { Card } from "react-native-paper";
+import { SvgUri, SvgXml } from "react-native-svg";
+import star from "../../../../assets/star";
+import open from "../../../../assets/open";
 
 const RestaurantCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.secondary}
@@ -14,32 +17,91 @@ const RestaurantCardCover = styled(Card.Cover)`
 
 const Title = styled(Text)`
   font-family: ${(props) => props.theme.fonts.heading};
-  font-weight: ${(props) => props.theme.fontWeights.bold};
-  padding: ${(props) => props.theme.space[3]};
+  font-size: ${(props) => props.theme.fontSizes.body};
   color: ${(props) => props.theme.colors.ui.success};
   `;
 
+  const Address = styled(Text)`
+    font-family: ${(props) => props.theme.fonts.body};
+    font-size: ${(props) => props.theme.fontSizes.caption};
+  `;
+
+  const Info = styled.View`
+    padding: ${(props) => props.theme.space[3]};  
+  `;
+  
+  const Rating = styled.View`
+    flex-direction: row;
+    padding-top: ${(props) => props.theme.space[2]};
+    padding-bottom: ${(props) => props.theme.space[2]};
+  `;
+
+    const Section = styled.View`
+    flex-direction: row;
+    align-items: center;
+  `;
+
+  const SectionEnd = styled.View`
+    flex: 1;
+    flex-direction: row;
+    justify-content: flex-end;
+  `;
 
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
 
     const {
-        name = "Kiko's Pizza Restaurant",
-        icon,
+        name = "Some Restaurant",
+        icon = "https://maps.gstatic.com/mapfiles/place_api/icons/v1/png_71/lodging-71.png",
         photos = [
             "https://www.foodiesfeed.com/wp-content/uploads/2019/02/messy-pizza-on-a-black-table.jpg",
         ],
         address = "100 some random street in the Milky Way",
-        isOpenNow = "Open @ 8:00 AM - 10:00 PM",
+        isOpenNow = true,
         rating = 4,
-        isClosedTemporarily,
+        isClosedTemporarily = true,
       
         
     } = restaurant;
 
+    const ratingArray = Array.from(new Array(Math.floor(rating)));
+    // console.log(ratingArray);
+
     return (
         <RestaurantCard elevation={5}>
           <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
+          <Info>
             <Title>{name}</Title>
+
+            <Section>
+
+              <Rating>
+                {ratingArray.map(() => (
+                  <SvgXml xml={star} width={20} height={20} />
+                ))}
+              </Rating>
+
+              <SectionEnd>
+                {isClosedTemporarily && (
+                  <Text variant="label" style={{ color: "red" }}>
+                    CLOSED TEMPORARILY
+                  </Text>
+                )}
+
+                <View style={{ paddingLeft: 16 }} />
+
+                {isOpenNow && <SvgXml xml={open} width={20} height={20} />}
+
+                <View style={{ paddingLeft: 16 }} />
+                
+                <Image style={{ width: 20, height: 20 }} source={{ uri: icon }} />
+              </SectionEnd>
+
+            </Section>
+
+
+            <Address>{address}</Address>
+
+          </Info>
         </RestaurantCard>
       );
 };
