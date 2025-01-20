2dcc7b8bd0de2275209d7055be3573fa25b7793e
KramDipal <francis.lapid@gmail.com>
2025-01-20 16:48:34 +0800

    Check point Getting Favourites per user

--- App.js
+++ App.js
@@ -56,13 +56,7 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <AuthenticationContextProvider>
-          <FavouritesContextProvider>
-            <LocationContextProvider>
-                <RestaurantsContextProvider>
-                  <Navigation />
-                </RestaurantsContextProvider>
-            </LocationContextProvider>
-          </FavouritesContextProvider>
+          <Navigation />
         </AuthenticationContextProvider>
       </ThemeProvider>
         <ExpoStatusBar style="auto" />
