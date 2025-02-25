185f211e0ac4054226208521681a0eebb9930d6d
KramDipal <francis.lapid@gmail.com>
2025-02-25 11:35:15 +0800

    check point: MTG Core two latest

--- src/features/account/screens/account.screen.js
+++ src/features/account/screens/account.screen.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
+import LottieView from "lottie-react-native";
 import { 
     AccountBackground, 
     AccountCover, 
     AccountContainer, 
     AuthButton,
+    AnimationWrapper,
     Title } from "../components/account.styles";
 
 import { Spacer } from "../../../components/spacer/spacer.component";
@@ -12,6 +13,16 @@ import { Spacer } from "../../../components/spacer/spacer.component";
 export const AccountScreen = ({ navigation }) => {
   return <AccountBackground>
         <AccountCover />
+        <AnimationWrapper>
+            <LottieView
+            style={{flex: 1}}
+            key="animation"
+            autoPlay
+            loop
+            resizeMode="cover"
+            source={require("../../../../assets/watermelon.json")}
+            />
+        </AnimationWrapper>
         <Title>Meals To Go</Title>
             <AccountContainer>
                 <AuthButton
